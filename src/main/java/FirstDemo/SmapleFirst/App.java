1595971439e1820ea719549abb821b9ff14782a6
Akhil Sharma <Akhil.Sharma@LXI.lumedx.com>
2020-03-26 12:59:46 +0530

    Devloper sample project

---
+++ src/main/java/FirstDemo/SmapleFirst/App.java
@@ -0,0 +1,13 @@
+package FirstDemo.SmapleFirst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello WorldDev!" );
+    }
+}
