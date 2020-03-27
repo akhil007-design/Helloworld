1907ca7b9f220ade1c24d86aeda141582434e2d4
akhil007-design <62534723+akhil007-design@users.noreply.github.com>
2020-03-27 15:40:52 +0530

    Update App.java
    Chnge Devcode

--- src/main/java/FirstDemo/SmapleFirst/App.java
+++ src/main/java/FirstDemo/SmapleFirst/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello WorldDev!" );
+        System.out.println( "Hello WorldDevChnage 27-03!" );
     }
 }
